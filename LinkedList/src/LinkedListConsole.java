7fb6cee5008aafe21f1a69483f4a49f8345432b7
Jordan Dukes <cyberdemon23@gmail.com>
2017-06-02 06:21:30 -0400

    kinda sorta graph

--- LinkedList/src/LinkedListConsole.java
+++ LinkedList/src/LinkedListConsole.java
@@ -1,42 +1,33 @@
 /**
  * Created by Jordan on 5/25/2017.
  */
-import jordan.arraygarbage.*;
+import jordan.arraygarbage.SortFunctions;
+import jordan.structures.Graph;
+import jordan.structures.MaxHeap;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class LinkedListConsole {
     public static void main(String[] arguments) {
-        int[] arrayOne, arrayTwo;
         Random rand = new Random();
 
         System.out.println("Please enter a number: ");
         Scanner in = new Scanner(System.in);
         int i = in.nextInt();
-        int k = in.nextInt();
 
-        arrayOne = new int[i + k];
-        arrayTwo = new int[k];
+        Graph graph = new Graph();
 
-        for(int j = 0; j < i; j++){
-            arrayOne[j] = (j * 15) + 1;
+        for(int k = 0; k < i; k++){
+            graph.add(rand.nextInt(100), new int[0]);
         }
 
-        for(int j = 0; j < k; j++){
-            arrayTwo[j] = rand.nextInt(100);
-        }
+        //System.out.println(graph.toString());
 
-        Arrays.sort(arrayTwo);
+//        System.out.println(String.format("Enter numbers from 1 - %d to find the shortest path between those nodes", i));
+//        int node1 = in.nextInt() - 1;
+//        int node2 = in.nextInt() - 1;
 
-        SortFunctions.printArray(arrayOne);
-        System.out.println();
-        SortFunctions.printArray(arrayTwo);
 
-        SortFunctions.sortedMerge(arrayOne, arrayTwo);
-        System.out.println();
-
-        SortFunctions.printArray(arrayOne);
     }
 }
